8820c1b7727c5ef12103ffedc5e9b868bba2c39d
RednIT2 <vanthanh011024@gmail.com>
2025-04-18 16:17:25 +0700

    update

--- server-side/server.js
+++ server-side/server.js
@@ -9,7 +9,7 @@ const shoeRoutes = require('./routes/shoeRoute');
 const userRoutes = require('./routes/userRoute');
 
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT;
 
 mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
@@ -17,15 +17,28 @@ mongoose.connect(process.env.MONGODB_URI, {
 });
 
 // Middleware
+
 const allowedOrigins = [
-    "http://localhost:3000", // dev local
-    "https://rednit-ecommerce-website-ngo-thanh-dats-projects.vercel.app", // production
+    "http://localhost:3000",
+    "https://rednit-ecommerce-website-ngo-thanh-dats-projects.vercel.app"
 ];
-const corsOptions = {
+
+app.use(cors({
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+        } else {
+        callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true,
+}));
+
+// Bổ sung header thủ công để xử lý preflight OPTIONS
+app.options('*', cors({
     origin: allowedOrigins,
     credentials: true,
-};
-app.use(cors(corsOptions));
+}));
 app.use(express.json());
 
 // Routes
@@ -35,7 +48,9 @@ app.use('/users', userRoutes);
 // Serve file ảnh từ thư mục uploads
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-
+app.get("/", (req, res) => {
+    res.send("Backend is running!");
+});
 
 // Khởi chạy server
 app.listen(port, () => {
