63d0e63914aa8dd926c132836c62c57bc49bd3a6
khang <khang@example.com>
2025-04-21 20:53:36 +0700

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   server-side/middlewares/authMiddleware.js

--- server-side/middlewares/authMiddleware.js
+++ server-side/middlewares/authMiddleware.js
@@ -1,19 +1,19 @@
-const jwt = require('jsonwebtoken');
+// const jwt = require('jsonwebtoken');
 
-// Middleware xác thực Access Token
-const authenticateToken = (req, res, next) => {
-  const token = req.headers.authorization?.split(' ')[1];
-  if (!token) {
-    return res.status(401).json({ message: 'Access token is missing or invalid.' });
-  }
+// // Middleware xác thực Access Token
+// const authenticateToken = (req, res, next) => {
+//   const token = req.headers.authorization?.split(' ')[1];
+//   if (!token) {
+//     return res.status(401).json({ message: 'Access token is missing or invalid.' });
+//   }
 
-  try {
-    const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET); // Sử dụng ACCESS_TOKEN_SECRET
-    req.user = decoded; // Lưu thông tin người dùng vào request
-    next();
-  } catch (error) {
-    res.status(403).json({ message: 'Invalid or expired access token.' });
-  }
-};
+//   try {
+//     const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET); // Sử dụng ACCESS_TOKEN_SECRET
+//     req.user = decoded; // Lưu thông tin người dùng vào request
+//     next();
+//   } catch (error) {
+//     res.status(403).json({ message: 'Invalid or expired access token.' });
+//   }
+// };
 
-module.exports = authenticateToken;
+// module.exports = authenticateToken;
